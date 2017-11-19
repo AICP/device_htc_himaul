6c3a0d4b71e5201c17ad8f2b148f1636f40898b2
Ali B <abittin@gmail.com>
2017-11-19 15:42:57 +0300

    aicpfy
    Change-Id: I7fe00efeef8378231d7dcb6131cb7b5945ef4470

---
+++ aicp.mk
@@ -0,0 +1,21 @@
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+$(call inherit-product, device/htc/himaul/full_himaul.mk)
+
+PRODUCT_NAME := aicp_himaul
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="htc_himaulatt" \
+    PRODUCT_NAME="himaulatt_na_gen_unlock" \
+    BUILD_FINGERPRINT="htc/himaulatt_na_gen_unlock/htc_himaulatt:6.0/MRA58K/669696.12:user/release-keys" \
+    PRIVATE_BUILD_DESC="3.35.617.12 CL669696 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Ali B (eyosen)"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/config/bootanimation.mk
